01745b5e9ce3efd9ee4192c6b877fbf3596fe9b6
Culganosi <culgan@gmail.com>
2022-12-08 14:47:04 -0500

    add Layout component

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,21 +1,24 @@
 import React from "react";
+import Layout from "../components/Layout";
 
-export default function About() {
+export default function About({ children }) {
   return (
-    <div>
-      <h1>About Page</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus,
-        alias?
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus,
-        alias?
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus,
-        alias?
-      </p>
-    </div>
+    <Layout>
+      <div>
+        <h1>About Page</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Necessitatibus, alias?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Necessitatibus, alias?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Necessitatibus, alias?
+        </p>
+      </div>
+    </Layout>
   );
 }
--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,10 +1,13 @@
 import React from "react";
+import Layout from "../../components/Layout";
 
-export default function Projects() {
+export default function Projects({ children }) {
   return (
-    <div>
-      <h2>Portfolio</h2>
-      <h3>Projects && Websites I've Made.</h3>
-    </div>
+    <Layout>
+      <div>
+        <h2>Portfolio</h2>
+        <h3>Projects && Websites I've Made.</h3>
+      </div>
+    </Layout>
   );
 }
