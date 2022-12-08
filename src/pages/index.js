a30f8a9a65d7d2969d417c37a1ede7fd540e89b1
Culganosi <culgan@gmail.com>
2022-12-08 15:13:59 -0500

    add styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,19 @@
+import { Link } from "gatsby";
 import * as React from "react";
 import Layout from "../components/Layout";
+import * as styles from "../styles/home.module.css";
 
 export default function Home() {
   return (
     <Layout>
-      <section>
+      <section className={styles.header}>
         <div>
           <h2>Design</h2>
           <h3>Develop & Deploy</h3>
           <p>Web Developer based in Montreal</p>
+          <Link className={styles.btn} to="/projects">
+            My Projects
+          </Link>
         </div>
       </section>
     </Layout>
--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Layout from "../../components/Layout";
+import * as styles from "../../styles/projects.module.css";
 
 export default function Projects({ children }) {
   return (
     <Layout>
-      <div>
+      <div className={styles.portfolio}>
         <h2>Portfolio</h2>
         <h3>Projects && Websites I've Made.</h3>
       </div>
