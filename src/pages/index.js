05e51b24c3cb6975e08503265227810f54e3754e
Culganosi <culgan@gmail.com>
2022-12-08 14:35:48 -0500

    add Navbar component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,15 @@
-import * as React from "react"
+import * as React from "react";
+import Navbar from "../components/Navbar";
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <section>
+      <Navbar />
+      <div>
+        <h2>Design</h2>
+        <h3>Develop & Deploy</h3>
+        <p>Web Developer based in Montreal</p>
+      </div>
+    </section>
+  );
 }
